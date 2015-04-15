2d66e9604fb19207b7d809f0ae9203cd67e2e051
Roger Ignazio <roger@puppetlabs.com>
2015-04-15 11:52:57 -0700

    add some spec tests
    This commit adds some basic rspec-puppet tests to ensure the module is
    behaving properly when various params are set.

--- spec/classes/waylon_spec.rb
+++ spec/classes/waylon_spec.rb
@@ -12,6 +12,107 @@ describe 'waylon' do
   end
 
   context 'with defaults for all parameters' do
-    it { should contain_class('waylon') }
+    it { should contain_class('waylon')            }
+    it { should contain_class('waylon::install')   }
+    it { should contain_class('waylon::config')    }
+    it { should contain_class('waylon::memcached') }
+    it { should contain_class('waylon::unicorn')   }
+    it { should contain_class('waylon::nginx')     }
+
+    it { should contain_rbenv__gem('waylon')  }
+    it { should contain_rbenv__gem('unicorn') }
+    it { should contain_package('nginx')      }
+  end
+
+  context 'with a custom rbenv_install_path' do
+    let :params do
+      {
+        :rbenv_install_path => '/opt/rbenv'
+      }
+    end
+
+    it { should contain_class('rbenv').with_install_dir('/opt/rbenv') }
+
+    it do
+      should contain_file('/etc/default/unicorn').with(
+        'content' => /APP_ROOT=\"\/opt\/rbenv/
+      )
+    end
+
+    it do
+      should contain_file('/etc/nginx/nginx.conf').with(
+        'content' => /root \/opt\/rbenv/
+      )
+    end
+  end
+
+  context 'with a custom ruby version' do
+    let :params do
+      {
+        :ruby_version => '9000'
+      }
+    end
+
+    it { should contain_rbenv__build('9000').with_global('true') }
+
+    it do
+      should contain_file('/etc/default/unicorn').with(
+        'content' => /\/versions\/9000\/lib\//
+      )
+    end
+
+    it do
+      should contain_file('/etc/nginx/nginx.conf').with(
+        'content' => /\/versions\/9000\/lib\//
+      )
+    end
+  end
+
+  context 'with a custom unicorn version' do
+    let :params do
+      {
+        :unicorn_version => '9000'
+      }
+    end
+
+    it { should contain_rbenv__gem('unicorn').with_version('9000') }
+  end
+
+  context 'with a custom waylon version' do
+    let :params do
+      {
+        :waylon_version => '9000'
+      }
+    end
+
+    it { should contain_rbenv__gem('waylon').with_version('9000') }
+
+    it do
+      should contain_file('/etc/default/unicorn').with(
+        'content' => /waylon-9000/
+      )
+    end
+
+    it do
+      should contain_file('/etc/nginx/nginx.conf').with(
+        'content' => /waylon-9000/
+      )
+    end
+  end
+
+  context 'on an unsupported operating system' do
+    let :facts do
+      {
+        :osfamily        => 'Debian',
+        :operatingsystem => 'Debian',
+        :lsbdistcodename => 'Jessie'
+      }
+    end
+
+    it do
+      expect {
+        catalogue
+      }.to raise_error(Puppet::Error, /rji-waylon does not support/)
+    end
   end
 end
