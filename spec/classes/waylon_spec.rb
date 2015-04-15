23a4ebc7a2abed89bbb3559f3954a0fffaa5c743
Roger Ignazio <roger@puppetlabs.com>
2015-04-15 14:09:02 -0700

    test for default versions of packages
    These tests reference the default versions of packages as specified in
    params.pp.

--- spec/classes/waylon_spec.rb
+++ spec/classes/waylon_spec.rb
@@ -19,9 +19,13 @@ describe 'waylon' do
     it { should contain_class('waylon::unicorn')   }
     it { should contain_class('waylon::nginx')     }
 
-    it { should contain_rbenv__gem('waylon')  }
-    it { should contain_rbenv__gem('unicorn') }
-    it { should contain_package('nginx')      }
+    it 'should install default versions of all packages' do
+      should contain_rbenv__build('2.1.5').with_global('true')
+      should contain_rbenv__gem('waylon').with_version('2.1.2')
+      should contain_rbenv__gem('waylon').with_ruby_version('2.1.5')
+      should contain_rbenv__gem('unicorn').with_ruby_version('2.1.5')
+      should contain_package('nginx')
+    end
 
     it 'should manage waylon.yml with hiera' do
       should contain_file('/usr/local/rbenv/versions/2.1.5/lib/ruby/gems/2.1.0/gems/waylon-2.1.2/config/waylon.yml').with(
