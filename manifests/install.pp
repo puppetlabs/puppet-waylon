43e2e610cbbf063cf49462774da23dc30267c3ff
Jake Spain <jake.spain+15749776@perforce.com>
2023-04-14 08:20:41 -0400

    Use ensure_packages to avoid duplicate resource declarations

--- manifests/install.pp
+++ manifests/install.pp
@@ -17,18 +17,14 @@ class waylon::install (
         # build-essential and libssl-dev are req'd for the rbenv ruby-build plugin.
         # libsasl2-dev and gettext are req'd for building native extensions for the
         # memcached gem.
-        package { ['build-essential', 'libssl-dev', 'libsasl2-dev', 'gettext']:
-          ensure => installed,
-          before => Class['rbenv'],
-        }
+        $_package_deps = ['build-essential', 'libssl-dev', 'libsasl2-dev', 'gettext']
+        ensure_packages($_package_deps)
       }
     }
     'centos': {
       # for rbenv
-      package { ['git-core', 'zlib', 'zlib-devel', 'gcc-c++', 'patch', 'readline', 'readline-devel', 'libyaml-devel', 'libffi-devel', 'openssl-devel', 'make', 'bzip2', 'autoconf', 'automake', 'libtool', 'bison', 'sqlite-devel']:
-        ensure => installed,
-        before => Class['rbenv'],
-      }
+      $_package_deps = ['git-core', 'zlib', 'zlib-devel', 'gcc-c++', 'patch', 'readline', 'readline-devel', 'libyaml-devel', 'libffi-devel', 'openssl-devel', 'make', 'bzip2', 'autoconf', 'automake', 'libtool', 'bison', 'sqlite-devel']
+      ensure_packages($_package_deps)
       # for bundle install - building memcached with native extensions
       package { 'cyrus-sasl-devel':
         ensure => installed,
